479e120bb7974e3ad9aa6386359f6abc5d8a7d9f
Eric Reizas <tuo17224@temple.edu>
2023-12-30 21:13:07 -0500

    Extracted sections with high schools links

--- crawl.py
+++ crawl.py
@@ -6,6 +6,7 @@ def get_phil_sd_hs_links():
     Creates list of Philadelphia school district high school links
     """
     req = requests.get("https://en.wikipedia.org/wiki/List_of_schools_of_the_School_District_of_Philadelphia")
-    print(req.status_code)
+    soup = BeautifulSoup(req.text,'html.parser')
+    link_sections = soup.find_all('div',attrs={'class':"div-col"})[3:6]
 
 get_phil_sd_hs_links()

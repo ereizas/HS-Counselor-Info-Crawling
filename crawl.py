0a155e8bf2edb8837a176c74412e12feb269d16d
Eric Reizas <tuo17224@temple.edu>
2023-12-31 12:02:41 -0500

    Extracted wikipedia domain suffix for each school

--- crawl.py
+++ crawl.py
@@ -5,8 +5,22 @@ def get_phil_sd_hs_links():
     """
     Creates list of Philadelphia school district high school links
     """
+    school_to_link = dict()
     req = requests.get("https://en.wikipedia.org/wiki/List_of_schools_of_the_School_District_of_Philadelphia")
     soup = BeautifulSoup(req.text,'html.parser')
     link_sections = soup.find_all('div',attrs={'class':"div-col"})[3:6]
+    for sect in link_sections:
+        schools_html=sect.find_all('li')
+        for school in schools_html:
+            school_str = str(school)
+            if 'href' in school_str:
+                wiki_str_ind = school_str.find('wiki/')+6
+                wiki_domain_suff = school_str[wiki_str_ind:school_str.find('\"',wiki_str_ind)]
+                print(wiki_domain_suff)
+            else:
+                #google search
+                pass
+
+            
 
 get_phil_sd_hs_links()

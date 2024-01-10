924ad5d7a7cb6b32cf7fbe976315f157245098ae
Eric Reizas <tuo17224@temple.edu>
2024-01-10 10:41:40 -0500

    Restrategized parsing to retrieve all high schools

--- crawl.py
+++ crawl.py
@@ -23,37 +23,39 @@ def get_phil_sd_hs_links():
     soup = BeautifulSoup(req.text,'html.parser')
     link_sections = soup.find_all('div',attrs={'class':"div-col"})[3:6]
     for sect in link_sections:
-        schools_html=sect.find_all('a')
+        schools_html=sect.find_all('li')
         for school in schools_html:
-            school_wiki__domain_suff = school.get('href')
-            if school_wiki__domain_suff:
-                school_wiki_req = requests.get("https://en.wikipedia.org"+school_wiki__domain_suff)
-                soup=BeautifulSoup(school_wiki_req.text,'html.parser')
-                links = soup.find_all('a')
-                hs_link = ''
-                #searches for school link in wikipedia page and then Google if not found
-                for link in links:
-                    school_str = school.text
-                    up_to_ind = school_str.find('School')
-                    school_str_len = len(school_str)
-                    if up_to_ind+5!=school_str_len-1:
-                        up_to_ind=school_str_len
-                    potential_link = link.get('href')
-                    if potential_link and 'philasd.org' in potential_link and 'www.philasd.org' not in potential_link and any(item.lower() in potential_link for item in school_str[:up_to_ind].split()):
-                        hs_link=potential_link
-                        if hs_link[-1]!='/':
-                            hs_link+='/'
-                        school_to_link[school.text]=hs_link
-                        break
-                if not hs_link:
-                    goog_srch_res = google_search_school(school.text)
-                    if goog_srch_res:
-                        school_to_link[school.text]=goog_srch_res
+            a_tag_portion = school.find('a')
+            school_wiki__domain_suff=None
+            if a_tag_portion:
+                school_wiki__domain_suff = a_tag_portion.get('href')
+                if school_wiki__domain_suff:
+                    school_wiki_req = requests.get("https://en.wikipedia.org"+school_wiki__domain_suff)
+                    soup=BeautifulSoup(school_wiki_req.text,'html.parser')
+                    links = soup.find_all('a')
+                    hs_link = ''
+                    #searches for school link in wikipedia page and then Google if not found
+                    for link in links:
+                        school_str = school.text
+                        up_to_ind = school_str.find('School')
+                        school_str_len = len(school_str)
+                        if up_to_ind+5!=school_str_len-1:
+                            up_to_ind=school_str_len
+                        potential_link = link.get('href')
+                        if potential_link and 'philasd.org' in potential_link and 'www.philasd.org' not in potential_link and any(item.lower() in potential_link for item in school_str[:up_to_ind].split()):
+                            hs_link=potential_link
+                            if hs_link[-1]!='/':
+                                hs_link+='/'
+                            school_to_link[school.text]=hs_link[:hs_link.find('.org/')+5]
+                            break
+                    if not hs_link:
+                        goog_srch_res = google_search_school(school.text)
+                        if goog_srch_res:
+                            school_to_link[school.text]=goog_srch_res
             else:
                 goog_srch_res = google_search_school(school.text)
                 if goog_srch_res:
                     school_to_link[school.text]=goog_srch_res
-    #End up missing 10 of the high schools in the district
     #print(len(school_to_link))
     for link in school_to_link:
         couns_req = requests.get(school_to_link[link]+'counselors-corner')
